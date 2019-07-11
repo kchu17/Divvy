45d0bc67efb2ac484baa2609220250e230b88cba
Sam Westerman <westerman@ibm.com>
2019-07-11 10:40:02 -0700

    added install

--- backend/install.sh
+++ backend/install.sh
@@ -1,6 +1,30 @@
-echo '    Installing Ruby Dependencies'
+echo '> Installing Ruby Dependencies'
 gem install sinatra
 gem install redis
+
+
+echo '> Installing the `crl` command'
+cat <<'EOF' >> ~/.bashrc
+crl () 
+{ 
+    if [ $1 = "GET" ]; then
+        curl -s $2;
+    else
+        curl -s -X $1 -H "Content-type: application/json" -d "$3" $2;
+    fi
+}
+EOF
+echo "export LOCAL='localhost:4567' # set the \$LOCAL variable" >> ~/.bashrc
+echo '-w "\n"' >> ~/.curlc
+source ~/.bashrc
+
+echo <<'EOF'
+USAGE: crl (GET/POST/PUT/DELETE) (URL) [DATA, in json format]
+	DATA is not used for 'GET' posts
+EXAMPLE:
+	crl GET $LOCAL/auth
+	crl POST $LOCAL/users/123456 '{"name": "hi", "age": 1234}'
+EOF
 
 # echo '    Installing Redis'
 # echo '    Install from (http://download.redis.io/redis-stable.tar.gz)'
