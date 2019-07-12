818b5ace1c8bb2a47887a386f66376d7de69590e
Sam Westerman <westerman@ibm.com>
2019-07-12 03:40:44 -0700

    you can now click on groups

--- frontend/DivvyFrontend/App.js
+++ frontend/DivvyFrontend/App.js
@@ -1,17 +1,19 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import Logo from './components/Logo';
+
 import MyRide from './MyRide';
 import AuthHomepage from './components/auth/AuthHomepage';
 import Login from './components/auth/Login';
 import SignUp from './components/auth/SignUp';
 import SearchForGroup from './components/hacks/SearchForGroup';
+import GroupDetailsScreen from './screens/GroupDetailsScreen';
 
 const AppNavigator = createStackNavigator({
-  Home: { screen: AuthHomepage},
+  Home: { screen: SearchForGroup/*AuthHomepage*/},
   PostAuthHomepage: { screen: MyRide },
   Login: { screen: Login },
   SignUp: { screen: SignUp },
   MyRide: { screen: MyRide },
+  GroupDetailsScreen: { screen: GroupDetailsScreen },
 });
 
 export default createAppContainer(AppNavigator);
