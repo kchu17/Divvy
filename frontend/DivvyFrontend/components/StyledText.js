c08f0f0b1fa3884d42dbbd4b30fb87fed27cf9bc
Matt Kong <mudsharkmk@gmail.com>
2019-07-11 19:37:12 -0700

    my ride barebones working

--- frontend/DivvyFrontend/components/StyledText.js
+++ frontend/DivvyFrontend/components/StyledText.js
@@ -6,3 +6,32 @@ export function MonoText(props) {
     <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />
   );
 }
+
+export const LText = (props) => (
+	<Text {...props} style={[{ fontSize: 20 }, props.style]} />
+);
+
+export const MonoLText = (props) => (
+	<Text {...props} style={[{ fontSize: 20 , fontFamily: 'monospace' }, props.style]} />
+);
+
+export const BLText = (props) => (
+	<Text {...props} style={[{ fontSize: 20, fontWeight: "bold" }, props.style]} />
+);
+
+export const XLText = (props) => (
+	<Text {...props} style={[{ fontSize: 25 }, props.style]} />
+);
+
+export const BXLText = (props) => (
+	<Text {...props} style={[{ fontSize: 25, fontWeight: "bold" }, props.style]} />
+);
+
+export const XXLText = (props) => (
+	<Text {...props} style={[{ fontSize: 30 }, props.style]} />
+);
+
+export const BXXLText = (props) => (
+	<Text {...props} style={[{ fontSize: 30, fontWeight: "bold" }, props.style]} />
+);
+
--- frontend/DivvyFrontend/screens/MyRideScreen.js
+++ frontend/DivvyFrontend/screens/MyRideScreen.js
@@ -1,30 +1,124 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+	View, StyleSheet, ScrollView, TouchableOpacity, Image, FlatList,
+	StatusBar, ActivityIndicator
+} from "react-native";
+import {
+	MonoText, LText, MonoLText, BLText, XLText, BXLText, XXLText, BXXLText
+} from "../components/StyledText";
+import { Person } from "../components/Touchables";
 
 const axios = require("axios");
-const DATABASE_URL = "localhost:4567";
+const DB_URL = "http://ec2-18-219-137-171.us-east-2.compute.amazonaws.com:4567";
 
 export default class MyRideScreen extends Component {
-	static navigationOptions = {
-		title: "My Ride"
-	};
+	static navigationOptions = { header: null };
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+		};
+		// axios.get(`${DB_URL}/auth`)
+		// 	.then(console.log)
+		// 	.catch(console.log);
+	}
+
+	componentDidMount() {
+		this.setState({ loading: false });
+	}
 
 	render() {
-		const rideToday = "";
+		const header = (
+			<View style={styles.header}>
+				<BXXLText style={styles.headerText}>My Ride</BXXLText>
+			</View>
+		);
+
+		const body = (this.state.loading ?
+			<View style={styles.body}><ActivityIndicator size="large" /></View> :
+			<View style={styles.body}>
+				<ScrollView>
+				<View>
+					<BXLText>Today</BXLText>
+				</View>
+
+				<TouchableOpacity style={styles.rideSummary}>
+					<BLText>Scheduled time:</BLText>
+					<MonoLText style={styles.timeText}>7:45 AM ~ 8:40 AM</MonoLText>
+				</TouchableOpacity>
+
+				<View>
+					<View>
+						<BLText>Real-time ETA:</BLText>
+						<MonoLText style={styles.timeText}>7:50 AM ~ 8:50 AM</MonoLText>
+					</View>
+					<View>
+						<FlatList
+							ListHeaderComponent={<BLText>Progress</BLText>}
+							data={[
+								{
+									key: "Bill",
+									pickedUp: true,
+									isMe: false,
+									profPicUrl: "https://catking.in/wp-content/uploads/2017/02/default-profile-1.png"
+								},
+								{ key: "May", pickedUp: true, isMe: false },
+								{ key: "Kevin", pickedUp: false, isMe: true },
+								{ key: "Sam", pickedUp: false, isMe: false }
+							]}
+							renderItem={({item}) => (
+								<Person name={item.key} pickedUp={item.pickedUp} isMe={item.isMe}/>
+							)}
+						/>
+					</View>
+				</View>
+				</ScrollView>
+			</View>
+		);
+
 		return (
-			<View>
-				<Text style={styles.test}>
-					this is my ride screen
-				</Text>
+			<View style={styles.screenWrapper}>
+				{header}
+				{body}
 			</View>
 		);
 	}
 }
 
+
 const styles = StyleSheet.create({
-	test: {
-		color: "red"
-	}
+	screenWrapper: { flex: 1 },
+		header: {
+			backgroundColor: "#cccccc",
+			flex: 1,
+			paddingTop: StatusBar.currentHeight,
+			justifyContent: "center",
+			alignItems: "center"
+		},
+			headerText: { color: "#444444" },
+		body: {
+			flex: 10,
+			padding: 15,
+		},
+			rideSummary: {
+				borderStyle: "solid",
+				borderWidth: 1,
+				borderRadius: 10,
+				padding: 10,
+				margin: 20,
+			},
+			person: {
+				borderWidth: 1,
+				borderStyle: "solid",
+				borderRadius: 10,
+				margin: 10,
+				padding: 10,
+			},
+
+	timeText: {
+		textAlign: "center",
+	},
 });
 
 
