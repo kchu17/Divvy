238bf809578cf782abb26cccd3162448f056d8c3
Sam Westerman <westerman@ibm.com>
2019-07-11 18:27:07 -0700

    this allows everyone to connect

--- backend/server.rb
+++ backend/server.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require 'json'
 
 set :show_exceptions, false
-set :host, `hostname -i` # allows everyone to connect
+set :host, '0.0.0.0' # allows everyone to connect
 
 
 error do
